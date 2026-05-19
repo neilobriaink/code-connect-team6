457973496e0011d59f7bb46fdbab6fe90553d36b
janemcgra <janemariemcgra@gmail.com>
2026-05-19 11:37:50 +0100

    adding job roles (#17)

--- routes/roles.js
+++ routes/roles.js
@@ -4,13 +4,19 @@ var router = express.Router();
 const JobRolesService = require('../services/jobRolesService');
 const rolesService = new JobRolesService();
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
+    const roles = rolesService.getJobRoles();
+    res.render('jobRolesList', { roles });
+});
 
-    const roles = rolesService.getJobRoles()
-    res.render('JobRolesList',
-        {roles}
-    )
-})
+router.get('/job-role', (req, res) => {
+    res.render('addJobRole');
+});
 
+router.post('/job-role', (req, res) => {
+    const { name, level, capability, description } = req.body;
+    rolesService.createJobRole(name, level, capability, description);
+    res.redirect('/roles');
+});
 
 module.exports = router;
--- services/jobRolesService.js
+++ services/jobRolesService.js
@@ -7,16 +7,23 @@ class JobRolesService {
     }
 
     getJobRoles() {
-            try {
-                const data = fs.readFileSync(this.filePath, 'utf8');
-                return JSON.parse(data);
-            } catch (err) {
-                console.error('Error reading employees:', err);
-                return [];
-            }
+        try {
+            const data = fs.readFileSync(this.filePath, 'utf8');
+            return JSON.parse(data);
+        } catch (err) {
+            console.error('Error reading job roles:', err);
+            return [];
         }
+    }
 
-    
+    createJobRole(name, level, capability, description) {
+        const roles = this.getJobRoles();
+        const id = roles.length > 0 ? Math.max(...roles.map(r => r.id)) + 1 : 1;
+        const newRole = { id, name, level, capability, description };
+        roles.push(newRole);
+        fs.writeFileSync(this.filePath, JSON.stringify(roles, null, 2));
+        return newRole;
+    }
 }
 
 module.exports = JobRolesService;
