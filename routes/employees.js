793bfba3eb0ccf12395d1eb395ad8d91e7cd47b2
Alanna Toner <atoner25@qub.ac.uk>
2026-05-19 09:59:19 +0100

    fix bug

--- routes/employees.js
+++ routes/employees.js
@@ -9,6 +9,20 @@ router.get('/', (req, res) => {
     res.render('employeeList', { employees });
 });
 
+// Update an employee by employee number form
+router.get('/update/:employeeNumber', (req, res) => {
+    const employee = employeeService.getEmployeeByNumber(parseInt(req.params.employeeNumber));
+    if (!employee) return res.status(404).send('Employee not found');
+    res.render('updateEmployee', { employee });
+});
+
+// Update an employee by employee number
+router.post('/update/:employeeNumber', (req, res) => {
+    const updatedEmployee = employeeService.updateEmployee(parseInt(req.params.employeeNumber), req.body);
+    if (!updatedEmployee) return res.status(404).send('Employee not found');
+    res.redirect('/employees');
+});
+
 // Create a new employee form
 router.get('/add', (req, res) => {
   res.render('addEmployee')
@@ -37,11 +51,15 @@ router.post('/add', validateEmployee, (req, res) => {
   res.redirect('/employees/');
 });
 
-// Delete an employee
-router.post('/delete/:id', (req, res) => {
-  const deleted = employeeService.deleteEmployee(parseInt(req.params.id));
-  if (!deleted) return res.status(404).send('Employee not found');
-  res.redirect('/employees');
+router.get('/:id', (req, res) => {
+    const employee = employeeService.getEmployeeById(req.params.id);
+    
+    if (!employee) {
+        return res.redirect('/employees');
+    }
+
+    res.render('employeeDetails', { employee });
 });
 
 module.exports = router;
+
